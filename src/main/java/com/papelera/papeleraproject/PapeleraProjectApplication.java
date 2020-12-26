59ca311d9d8d9a17623f0fe55a8cd498f6487469
leanuema <leanuema@hotmail.com>
2020-12-26 15:12:21 -0300

    feat(structure): add modules and dependecies

---
+++ src/main/java/com/papelera/papeleraproject/PapeleraProjectApplication.java
@@ -0,0 +1,12 @@
+package com.papelera.papeleraproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapeleraProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapeleraProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/papelera/papeleraproject/PapeleraProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papelera.papeleraproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapeleraProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
